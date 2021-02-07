7bd1f00b420be79af20b1f4114b4073091e0b062
shoprogram <shoprogramsa@gmail.com>
2021-02-07 16:32:21 +0900

    login,signup:SPAcomplete

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Logo from './img/logo.png';
+import {useDispatch} from 'react-redux';
+import {push} from 'connected-react-router';
 import Styles from './StyleComponents/Header.module.css';
 
 const HeaderComponent = () => {
+  const dispatch = useDispatch();
   return (
     <div className={ Styles.headerStyle } >
       <div className={ Styles.headerTop }>
         <img src={ Logo } alt="logo" className={ Styles.logo }/>
         <div className={ Styles.headerTopNav}>
-          <a href="#" className={ Styles.logIn}>ログイン</a>
-          <a href="#" className={ Styles.signIn}>新規登録</a>
+          <a className={ Styles.logIn} onClick={() => dispatch(push("login"))}>ログイン</a>
+          <a className={ Styles.signIn} onClick={() => dispatch(push("signup"))}>新規登録</a>
         </div>
       </div>
       
